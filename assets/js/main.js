d465d49a9b19ba6b6cbe05e97399ddb2f8ffa7e6
xtrp <36894883+xtrp@users.noreply.github.com>
2018-11-07 16:08:58 +0000

    got newsapi working

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// format date function taken from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + ' ' + day + ' ' + year;
+}
