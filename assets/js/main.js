c2a1bc43b28b2c3c5a6c85e516282fc026382905
xtrp <36894883+xtrp@users.noreply.github.com>
2018-11-08 21:28:28 +0000

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,16 @@ function formatDate(date) {
 
   return monthNames[monthIndex] + ' ' + day + ', ' + year;
 }
+
+// get query variable taken from css-tricks.com
+function getQueryVariable(variable){
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){
+      return pair[1];
+    }
+  }
+  return(false);
+}
